d473d08f1cdb66c669b690cce26cf387d17328af
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-06 04:32:45 +0530

    [Tushar Vidwans] Using LinkedList to do stack operation

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Stack_day14
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Peek();
+            stack.Pop();
+            stack.Peek();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_day14
+{
+    public class Stack<T>
+    {
+
+        LinkedList<T> linkedList;
+        
+        //stack constructor is used to declare linked list
+
+        public Stack()
+        {
+            this.linkedList = new LinkedList<T>();
+        }
+
+
+        //push method is used in stack operation to push the data of generic type
+        public void Push(T data)
+        {
+            linkedList.AddFirst(data);
+        }
+
+
+        //pop method is last in first out process where the last element is popped out first and deletes the element
+        public void Pop()
+        {
+            linkedList.RemoveFirst();
+        }
+
+        //peek method is to show to first element in the linked list, it does not delete the node from the stack memory
+        public void Peek()
+        {
+            Console.WriteLine(linkedList.First());
+        }
+
+    }
+}
